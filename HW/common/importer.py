9be85de70c0675c94b781be5cb38736e8cb8dc14
friendlyevil <FriendlyEvil@yandex.ru>
2021-10-08 12:44:52 +0300

    add error message to importer

--- HW/common/importer.py
+++ HW/common/importer.py
@@ -13,6 +13,10 @@ class Importer:
     @staticmethod
     def import_function(module, function_name, globals):
         if isinstance(function_name, tuple) or isinstance(function_name, list):
-            globals[function_name[1]] = module.__dict__[function_name[0]]
+            globals[function_name[1]] = module.__dict__.get(function_name[0])
+            if globals[function_name[1]] is None:
+                raise ValueError(f'object "{function_name[0]}" not found in your code')
         else:
-            globals[function_name] = module.__dict__[function_name]
+            globals[function_name] = module.__dict__.get(function_name)
+            if globals[function_name] is None:
+                raise ValueError(f'object "{function_name}" not found in your code')
